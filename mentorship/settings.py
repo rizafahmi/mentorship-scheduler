7cf22fabc24c09d4da729889aa1fcf2247526d3b
Riza Fahmi <rizafahmi@gmail.com>
2021-06-07 20:10:42 +0700

    Add static file

--- mentorship/settings.py
+++ mentorship/settings.py
@@ -120,9 +120,12 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS=['.herokuapp.com']
+ALLOWED_HOSTS=['*']
