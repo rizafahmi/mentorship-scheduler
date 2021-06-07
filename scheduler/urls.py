062c8d09247a2fdb811cd00ce1a80e1c6525f836
Riza Fahmi <rizafahmi@gmail.com>
2021-06-07 21:01:34 +0700

    Finish template/mockup

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('mentor/', views.mentor, name='create_mentor'),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "scheduler/index.html")
+
+def create(request):
+    return render(request, "scheduler/create.html")
+
+def mentor(request):
+    return render(request, "scheduler/mentor.html")
